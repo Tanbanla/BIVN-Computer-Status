77002d5f82f94d77ea1c3ee43046ed7e06b26c56
Nguyen Duy Khanh <tungvipduong@gmail.com>
2025-09-11 22:22:01 +0700

    hehe

---
+++ lib/Common/CommonColor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Common{
+    //color
+  static var primaryColor = const Color(0xff296e48);
+  static var blackColor = Colors.black54;
+  static var greenColor = const Color.fromARGB(255, 156, 179, 131);
+  static var grayColor = Colors.grey;
+}
